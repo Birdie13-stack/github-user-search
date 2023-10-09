d57d9226b87728abe27267f4d90c48c3c9cd3f0c
sirhmvfx <sirhmvfx@gmail.com>
2023-10-09 11:37:35 +0100

    fix search field

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,6 +1,6 @@
 import React from "react";
 import moon from "../assets/moon.png";
-import search from "../assets/search.png";
+import Search from "../assets/search.png";
 import Card from "../Components/Card";
 import "../App.css";
 
@@ -11,15 +11,28 @@ function HomePage() {
         <div className="title-side">
           <h1 className="title">devfinder</h1>
 
-          <label htmlFor="">
+          <label className="theme" htmlFor="">
             <input type="checkbox" name="" id="" /> Dark
             <img src={moon} alt="" />
           </label>
         </div>
-
-        <input type="text" placeholder="Search Github username..." />
       </div>
-      <Card></Card>
+
+      <div className="search-container">
+        <div className="search-field">
+          <img src={Search} alt="" />
+          <input
+            className="search"
+            type="text"
+            placeholder="Search Github username..."
+          />
+        </div>
+        <button>Search</button>
+      </div>
+
+      <div className="card-container">
+        <Card />
+      </div>
     </div>
   );
 }
